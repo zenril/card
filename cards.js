e00bad6a0cc847561355107b60e260cc2c637991
Fax <aaron@aaron-m.co.nz>
2025-11-06 03:11:28 +1300

    fixed

--- cards.js
+++ cards.js
@@ -2,8 +2,14 @@ const cardData = {
     width: 700,
     height: 1500,
     cards: [
-      "./cards/a-700x1500.jpg",
-      "./cards/b-700x1500.jpg"
+      "./cards/Audit.png",
+      "./cards/Cryptic_Instructions.png",
+      "./cards/Invasive_Weeds.png",
+      "./cards/Lucky_Read.png",
+      "./cards/Overtime.png",
+      "./cards/Redact.png",
+      "./cards/Synergy.png",
+      "./cards/Whispering_Static.png"
     ],
     back: "./cards/back.png"
   };
